5d8be300609d019068d678cdcb71b8195fe3c335
droofdd <48821680+droofdd@users.noreply.github.com>
2019-11-01 14:10:03 +0200

    3rd commit

--- server.js
+++ server.js
@@ -5,12 +5,23 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 
+const passport = require('passport');
+const session = require('express-session');
+
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+}));
+
+app.use(passport.initialize());
 
 app.set('view engine', 'pug');
 
