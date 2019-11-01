cb1309cf0f92e6ace796e63737a97e29647742be
droofdd <48821680+droofdd@users.noreply.github.com>
2019-11-01 14:51:50 +0200

    5

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 'use strict';
 
 require('dotenv').config()
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const express = require('express');
+const bodyParser = require('body-parser');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const passport = require('passport');
 const session = require('express-session');
+const ObjectID = require('mongodb').ObjectID;
+const mongo = require('mongodb').MongoClient;
 
 const app = express();
 
@@ -25,29 +27,40 @@ app.use(passport.initialize());
 
 app.set('view engine', 'pug');
 
-const ObjectID = require('mongodb').ObjectID;
 
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
+
+
 
 passport.deserializeUser((id, done) => {
-  /* db.collection('users').findOne(
-    {_id: new ObjectID(id)},
-      (err, doc) => {
-        done(null, doc);
-      }
-  ); */
-  done(null, null);
+  db.collection('users').findOne(
+    { _id: new ObjectID(id) },
+    (err, doc) => {
+      done(null, doc);
+    }
+  );
 });
 
 
 app.route('/')
   .get((req, res) => {
     //res.sendFile(process.cwd() + '/views/index.html');
-    res.render(process.cwd() + '/views/pug/index', {title: 'Hello', message: 'Please login'});
+    res.render(process.cwd() + '/views/pug/index', { title: 'Hello', message: 'Please login' });
   });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Listening on port " + process.env.PORT);
+
+mongo.connect(process.env.DATABASE, (err, db) => {
+  if (err) {
+    console.log('Database error: ' + err);
+  } else {
+    console.log('Successful database connection');
+    passport.serializeUser((user, done) => {
+      done(null, user._id);
+    });
+
+    app.listen(process.env.PORT || 3000, () => {
+      console.log("Listening on port " + process.env.PORT);
+    });
+  }
 });
+
+
